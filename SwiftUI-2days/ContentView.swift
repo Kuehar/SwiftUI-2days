2ccd690ae1c2ab1d0d11594605d290a226804d68
kuehar <green95251@gmail.com>
2021-06-13 14:48:23 +0900

    added VStack and Button

--- SwiftUI-2days/ContentView.swift
+++ SwiftUI-2days/ContentView.swift
@@ -9,8 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, SwiftUI!!")
-            .padding()
+        VStack {
+            Text("Hello, SwiftUI!!")
+                .fontWeight(.heavy)
+                .padding()
+        
+        Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Button")/*@END_MENU_TOKEN@*/
+        }
+        }
     }
 }
 
