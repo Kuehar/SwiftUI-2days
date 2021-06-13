26613cb95f5fd55353a67a0dd5fd56dafc2fdad5
kuehar <green95251@gmail.com>
2021-06-13 14:44:25 +0900

    Initial Commit

---
+++ SwiftUI-2days/SwiftUI_2daysApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_2daysApp.swift
+//  SwiftUI-2days
+//
+//  Created by kuehar on 2021/06/13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_2daysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
